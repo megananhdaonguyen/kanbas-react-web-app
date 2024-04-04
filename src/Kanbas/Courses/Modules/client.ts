230e73f3293fbfc68100bfa044c4b2d0329e23c1
megananhdaonguyen <nguyen.meg@northeastern.edu>
2024-04-04 04:19:49 -0400

    reconfiguring remote env

--- src/Kanbas/Courses/Modules/client.ts
+++ src/Kanbas/Courses/Modules/client.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
-const COURSES_API = "https://kanbas-node-server-app.onrender.com/api/courses";
-const MODULES_API = "https://kanbas-node-server-app.onrender.com/api/modules";
+const API_BASE = process.env.REACT_APP_API_BASE;
+const COURSES_API = `${API_BASE}/api/courses`;
+const MODULES_API = `${API_BASE}/api/modules`;
 export const deleteModule = async (moduleId: any) => {
   const response = await axios
     .delete(`${MODULES_API}/${moduleId}`);
--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -8,9 +8,10 @@ import store from "./store";
 import { Provider } from "react-redux";
 import axios from "axios";
 
+const API_BASE = process.env.REACT_APP_API_BASE;
 function Kanbas() {
   const [courses, setCourses] = useState<any[]>([]);
-  const COURSES_API = "https://kanbas-node-server-app.onrender.com/api/courses";
+  const COURSES_API = `${API_BASE}/api/courses`;
   const findAllCourses = async () => {
     const response = await axios.get(COURSES_API);
     setCourses(response.data);
